2c5b9b7977a104de6df31edb7f218e9a31814713
Sapir Shemer <lesscomplexity@gmail.com>
2021-01-04 20:09:21 +0200

    Autodetect 32-64 bit modes

--- CPPShift.h
+++ CPPShift.h
@@ -4,6 +4,20 @@
 #include <cstddef>
 #endif
 
+// Check windows
+#if _WIN32 || _WIN64
+#if !_WIN64
+#define CPPSHIFT_32BIT_MODE
+#endif
+#endif
+
+// Check GCC
+#if __GNUC__
+#if !(__x86_64__ && __ppc64__)
+#define CPPSHIFT_32BIT_MODE
+#endif
+#endif
+
 namespace CPPShift {
 #ifdef CPPSHIFT_32BIT_MODE
 	typedef unsigned int SIZE;
